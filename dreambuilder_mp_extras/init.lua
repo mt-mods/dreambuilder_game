d8650130c06100834f7ec74babde5f796ad5eae2
Vanessa Ezekowitz <vanessaezekowitz@gmail.com>
2016-04-01 20:24:56 -0400

    disable default lavacooling code and set interval + chance to 5, 15.

--- dreambuilder_mp_extras/init.lua
+++ dreambuilder_mp_extras/init.lua
@@ -1,3 +1,7 @@
+-- This mod contains a number of features specific to dreambuilder
+-- such as mesh-based apples, 16-slot hotbar, some color settings, etc.
+--
+-- by Vanessa Ezekowitz
 
 local hotbar_size = minetest.setting_get("hotbar_size") or 16
 
@@ -26,3 +30,30 @@ minetest.register_chatcommand("hotbar", {
 	end,
 })
 
+default.gui_bg = "bgcolor[#08080899;false]"
+
+default.gui_survival_form = "size[8,8.5]"..
+			default.gui_bg..
+			default.gui_bg_img..
+			default.gui_slots..
+			"list[current_player;main;0,4.25;8,1;]"..
+			"list[current_player;main;0,5.5;8,3;8]"..
+			"list[current_player;craft;1.75,0.5;3,3;]"..
+			"list[current_player;craftpreview;5.75,1.5;1,1;]"..
+			"image[4.75,1.5;1,1;gui_furnace_arrow_bg.png^[transformR270]"..
+			"listring[current_player;main]"..
+			"listring[current_player;craft]"..
+			default.get_hotbar_bg(0,4.25)
+
+default.cool_lava = function (pos, node) end
+
+minetest.register_abm({
+	nodenames = {"default:lava_source", "default:lava_flowing"},
+	neighbors = {"group:water"},
+	interval = 5,
+	chance = 15,
+	catch_up = false,
+	action = function(...)
+		default.cool_lava(...)
+	end,
+})
