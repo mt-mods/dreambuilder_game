eda0e1de79cd999e61fed44301dbc2a2cee5bf47
Vanessa Ezekowitz <vanessaezekowitz@gmail.com>
2017-04-05 11:35:00 -0400

    update pipeworks

--- pipeworks/item_transport.lua
+++ pipeworks/item_transport.lua
@@ -284,6 +284,7 @@ luaentity.register_entity("pipeworks:tubed_item", {
 					end
 				else
 					if minetest.get_item_group(rev_node.name,"tube") == 1 then
+						print("[Pipeworks] Warning - tubed item had to reverse direction at "..minetest.pos_to_string(self.start_pos))
 						velocity = vector.multiply(velocity, -1)
 						self:setvelocity(velocity)
 					else
