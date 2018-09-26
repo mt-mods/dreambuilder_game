514a4421ffca5c9b585fdce7d82c7e24ef9fd18d
Vanessa Dannenberg <vanessa.e.dannenberg@gmail.com>
2018-09-25 23:02:26 -0400

    update signs_lib and street signs

--- street_signs/init.lua
+++ street_signs/init.lua
@@ -515,7 +515,8 @@ minetest.register_node("street_signs:sign_basic_top_only", {
 local colors = {
 	{ "green",  "f", "dye:green",  "dye:white" },
 	{ "blue",   "f", "dye:blue",   "dye:white" },
-	{ "yellow", "0", "dye:yellow", "dye:black" }
+	{ "yellow", "0", "dye:yellow", "dye:black" },
+	{ "orange", "0", "dye:orange", "dye:black" }
 }
 
 for _, c in ipairs(colors) do
@@ -778,7 +779,7 @@ cbox = {
 }
 
 minetest.register_node("street_signs:sign_warning_3_line", {
-	description = "Basic US diamond-shaped \"warning\" sign (3-line)",
+	description = "Basic US diamond-shaped \"warning\" sign (3-line, yellow)",
 	paramtype = "light",
 	sunlight_propagates = true,
 	paramtype2 = "facedir",
@@ -822,7 +823,7 @@ minetest.register_node("street_signs:sign_warning_3_line", {
 })
 
 minetest.register_node("street_signs:sign_warning_4_line", {
-	description = "Basic US diamond-shaped \"warning\" sign (4-line)",
+	description = "Basic US diamond-shaped \"warning\" sign (4-line, yellow)",
 	paramtype = "light",
 	sunlight_propagates = true,
 	paramtype2 = "facedir",
@@ -864,6 +865,95 @@ minetest.register_node("street_signs:sign_warning_4_line", {
 		}
 	}
 })
+
+minetest.register_node("street_signs:sign_warning_orange_3_line", {
+	description = "Basic US diamond-shaped \"warning\" sign (3-line, orange)",
+	paramtype = "light",
+	sunlight_propagates = true,
+	paramtype2 = "facedir",
+	drawtype = "mesh",
+	node_box = cbox,
+	selection_box = cbox,
+	mesh = "street_signs_warning.obj",
+	tiles = { "street_signs_warning_orange.png" },
+	inventory_image = "street_signs_warning_orange_3_line_inv.png",
+	groups = {choppy=2, dig_immediate=2},
+	default_color = "0",
+	on_construct = function(pos) 
+		street_signs.construct_sign(pos)
+	end,
+	on_destruct = function(pos)
+		street_signs.destruct_sign(pos)
+	end,
+	on_receive_fields = function(pos, formname, fields, sender)
+		street_signs.receive_fields(pos, formname, fields, sender)
+	end,
+	on_punch = function(pos, node, puncher)
+		street_signs.update_sign(pos)
+	end,
+	number_of_lines = 3,
+	horiz_scaling = 1.75,
+	vert_scaling = 1.75,
+	line_spacing = 1,
+	font_size = 15,
+	x_offset = 6,
+	y_offset = 19,
+	chars_per_line = 15,
+	entity_info = {
+		mesh = "street_signs_warning_entity.obj",
+		yaw = {
+			0,
+			math.pi / -2,
+			math.pi,
+			math.pi / 2,
+		}
+	}
+})
+
+minetest.register_node("street_signs:sign_warning_orange_4_line", {
+	description = "Basic US diamond-shaped \"warning\" sign (4-line, orange)",
+	paramtype = "light",
+	sunlight_propagates = true,
+	paramtype2 = "facedir",
+	drawtype = "mesh",
+	node_box = cbox,
+	selection_box = cbox,
+	mesh = "street_signs_warning.obj",
+	tiles = { "street_signs_warning_orange.png" },
+	inventory_image = "street_signs_warning_orange_4_line_inv.png",
+	groups = {choppy=2, dig_immediate=2},
+	default_color = "0",
+	on_construct = function(pos) 
+		street_signs.construct_sign(pos)
+	end,
+	on_destruct = function(pos)
+		street_signs.destruct_sign(pos)
+	end,
+	on_receive_fields = function(pos, formname, fields, sender)
+		street_signs.receive_fields(pos, formname, fields, sender)
+	end,
+	on_punch = function(pos, node, puncher)
+		street_signs.update_sign(pos)
+	end,
+	number_of_lines = 4,
+	horiz_scaling = 1.75,
+	vert_scaling = 1.75,
+	line_spacing = 1,
+	font_size = 15,
+	x_offset = 6,
+	y_offset = 25,
+	chars_per_line = 15,
+	entity_info = {
+		mesh = "street_signs_warning_entity.obj",
+		yaw = {
+			0,
+			math.pi / -2,
+			math.pi,
+			math.pi / 2,
+		}
+	}
+})
+
 
 local signs_text_on_activate
 
