9e7ec9ecf03ff4a868a545a1f5659b315e067852
Vanessa Dannenberg <vanessa.e.dannenberg@gmail.com>
2018-11-08 22:29:24 -0500

    update roads and add license

--- concretestairs/init.lua
+++ concretestairs/init.lua
@@ -2,9 +2,9 @@
 	StreetsMod: Concrete stairs (compatible to circular saw)
 ]]
 if rawget(_G, "register_stair_slab_panel_micro")
-and type(register_stair_slab_panel_micro) == "function" and streets.extendedBy.prefab == false then
-	register_stair_slab_panel_micro("streets", "concrete", "streets:concrete", {cracky=2}, {"basic_materials_concrete_block.png"}, "Concrete", "concrete", nil)
-	table.insert(circular_saw.known_stairs,"streets:concrete")
+and type(register_stair_slab_panel_micro) == "function" then
+	register_stair_slab_panel_micro("streets", "concrete", "basic_materials:concrete_block", {cracky=2}, {"basic_materials_concrete_block.png"}, "Concrete", "concrete", nil)
+	table.insert(circular_saw.known_stairs,"basic_materials:concrete_block")
 	minetest.register_alias("stairs:stair_concrete","streets:stair_concrete")
 else
 	minetest.register_alias("stairs:stair_concrete","prefab:concrete_stair")
