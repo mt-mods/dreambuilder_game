d1a8445e01ba1be6ee535de7c2b04d6fc6130833
Vanessa Dannenberg <vanessa.e.dannenberg@gmail.com>
2021-03-27 20:59:31 -0400

    update pipeworks

--- mods/pipeworks/tube_registration.lua
+++ mods/pipeworks/tube_registration.lua
@@ -26,6 +26,11 @@ local texture_mt = {
 	end
 }
 
+-- This will remove any semi-transparent pixels
+-- because that is still buggy in Minetest, force this as default
+local texture_alpha_mode = minetest.features.use_texture_alpha_string_modes
+	and "clip" or true
+
 local register_one_tube = function(name, tname, dropname, desc, plain, noctrs, ends, short, inv, special, connects, style)
 	noctrs = noctrs or default_noctrs
 	setmetatable(noctrs, texture_mt)
@@ -82,6 +87,7 @@ local register_one_tube = function(name, tname, dropname, desc, plain, noctrs, e
 		description = tubedesc,
 		drawtype = "nodebox",
 		tiles = outimgs,
+		use_texture_alpha = texture_alpha_mode,
 		sunlight_propagates = true,
 		inventory_image = iimg,
 		wield_image = iimg,
@@ -107,6 +113,18 @@ local register_one_tube = function(name, tname, dropname, desc, plain, noctrs, e
 			connect_sides = {front = 1, back = 1, left = 1, right = 1, top = 1, bottom = 1},
 			priority = 50
 		},
+		on_punch = function(pos, node, player, pointed)
+			local playername = player:get_player_name()
+			if minetest.is_protected(pos, playername) and not minetest.check_player_privs(playername, {protection_bypass=true}) then
+				return minetest.node_punch(pos, node, player, pointed)
+			end
+			if pipeworks.check_and_wear_hammer(player) then
+				local wieldname = player:get_wielded_item():get_name()
+				pipeworks.logger(string.format("%s struck a tube at %s with %s to break it.", playername, minetest.pos_to_string(pos), wieldname))
+				pipeworks.break_tube(pos)
+			end
+			return minetest.node_punch(pos, node, player, pointed)
+		end,
 		after_place_node = pipeworks.after_place,
 		after_dig_node = pipeworks.after_dig,
 		on_rotate = false,
