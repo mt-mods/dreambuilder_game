577dae19b8d546fe3426bf9ab9aee1f2667803d6
unknown <24964441+wsor4035@users.noreply.github.com>
2021-12-26 00:35:08 -0500

    first pass submodules

--- mods/building_blocks/init.lua
+++
@@ -1,6 +0,0 @@
-local modpath = minetest.get_modpath("building_blocks")
-
-dofile(modpath.."/alias.lua")
-dofile(modpath.."/node_stairs.lua")
-dofile(modpath.."/others.lua")
-dofile(modpath.."/recipes.lua")
