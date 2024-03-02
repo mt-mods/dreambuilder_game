bd34412f4009a31492728f5d6b56b2a1eef183fe
wsor4035 <24964441+wsor4035@users.noreply.github.com>
2024-03-02 12:06:54 -0500

    attempt on hollytrees v2

--- mods/dreambuilder_backcompat/init.lua
+++ mods/dreambuilder_backcompat/init.lua
@@ -1,9 +1,9 @@
 if not minetest.get_modpath("hollytree") then
-    minetest.register_alias("hollytree:sapling", "default:sapling")
-    minetest.register_alias("hollytree:trunk","default:tree")
-    minetest.register_alias("hollytree:wood","default:wood")
+    minetest.register_alias("hollytree:sapling", "moretrees:apple_tree_sapling")
+    minetest.register_alias("hollytree:trunk","moretrees:apple_tree_trunk")
+    minetest.register_alias("hollytree:wood","moretrees:apple_tree_planks")
     minetest.register_alias("hollytree:leaves","default:leaves")
     --minetest.register_alias(,)
 
-    stairsplus:register_stair_alias("hollytree", "wood", "default", "wood")
+    stairsplus:register_stair_alias("hollytree", "wood", "moretrees", "apple_tree_planks")
 end
