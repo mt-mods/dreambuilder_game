df94c4e632f634c3821674d9a6c8d5e6ed0aba0f
wsor4035 <24964441+wsor4035@users.noreply.github.com>
2024-03-02 12:18:06 -0500

    alias ebony

--- mods/dreambuilder_backcompat/init.lua
+++ mods/dreambuilder_backcompat/init.lua
@@ -7,3 +7,13 @@ if not minetest.get_modpath("hollytree") then
 
     stairsplus:register_alias_all("hollytree", "wood", "moretrees", "apple_tree_planks")
 end
+
+if not minetest.get_modpath("ebony") then
+    minetest.register_alias("ebony:sapling", "moretrees:apple_tree_sapling")
+    minetest.register_alias("ebony:trunk","moretrees:apple_tree_trunk")
+    minetest.register_alias("ebony:wood","moretrees:apple_tree_planks")
+    minetest.register_alias("ebony:leaves","default:leaves")
+    --minetest.register_alias(,)
+
+    stairsplus:register_alias_all("ebony", "wood", "moretrees", "apple_tree_planks")
+end
