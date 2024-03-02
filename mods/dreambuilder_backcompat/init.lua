3edf435f59646940a88b40e3da791da8659674e0
wsor4035 <24964441+wsor4035@users.noreply.github.com>
2024-03-02 12:30:08 -0500

    more ebony things

--- mods/dreambuilder_backcompat/init.lua
+++ mods/dreambuilder_backcompat/init.lua
@@ -13,7 +13,10 @@ if not minetest.get_modpath("ebony") then
     minetest.register_alias("ebony:trunk","moretrees:apple_tree_trunk")
     minetest.register_alias("ebony:wood","moretrees:apple_tree_planks")
     minetest.register_alias("ebony:leaves","default:leaves")
-    --minetest.register_alias(,)
+    minetest.register_alias("ebony:creeper","moretrees:apple_tree_trunk")
+    minetest.register_alias("ebony:creeper_leaves","default:leaves")
+    minetest.register_alias("ebony:liana","air")
+    minetest.register_alias("ebony:permismmon","air")
 
     stairsplus:register_alias_all("ebony", "wood", "moretrees", "apple_tree_planks")
 end
