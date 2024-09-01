7e5673faac75bcd28149c9eaba00d817b6d60f8d
wsor4035 <24964441+wsor4035@users.noreply.github.com>
2024-08-31 22:07:32 -0400

    fix facade override bug

--- mods/dreambuilder_tweaks/src/facade.lua
+++ mods/dreambuilder_tweaks/src/facade.lua
@@ -1,6 +1,6 @@
 for k, v in pairs(minetest.registered_nodes) do
     if v.mod_origin=="facade" and k~="facade:shaper" then
-        local groups = table.copy(k.groups or {})
+        local groups = table.copy(v.groups or {})
         groups.not_in_creative_inventory = 1
         minetest.override_item(k, {
             groups = groups
