de1d477ad4bde32dff7d2d0922579a0fc8dc863d
2424barrier <2424barrier.minetest@gmail.com>
2025-11-11 22:39:18 +0000

    Fixed duplicate tree listings and switched to 'core' namespace in /mods/dreambuilder_backcompat/src/cooltrees.lua

--- mods/dreambuilder_backcompat/src/cooltrees.lua
+++ mods/dreambuilder_backcompat/src/cooltrees.lua
@@ -12,7 +12,6 @@ local treemodnames = {
     "clementinetree",
     "palm",
     "oak",
-    "bamboo",
     "sequoia",
     "jacaranda",
     "plumtree",
@@ -22,33 +21,31 @@ local treemodnames = {
     "clementine",
     "cherry",
     "hollytree",
-    "ebony",
     "pineapple",
-    "palm",
     "ebony",
 }
 
 for _,modname in pairs(treemodnames) do
-    if not minetest.get_modpath(modname) then
-        minetest.register_alias(modname .. ":sapling", "moretrees:apple_tree_sapling")
-        minetest.register_alias(modname .. ":trunk","moretrees:apple_tree_trunk")
-        minetest.register_alias(modname .. ":wood","moretrees:apple_tree_planks")
-        minetest.register_alias(modname .. ":leaves","default:leaves")
-        minetest.register_alias(modname .. ":fence","default:fence_wood")
-        minetest.register_alias(modname .. ":fence_rail","default:fence_rail_wood")
+    if not core.get_modpath(modname) then
+        core.register_alias(modname .. ":sapling", "moretrees:apple_tree_sapling")
+        core.register_alias(modname .. ":trunk","moretrees:apple_tree_trunk")
+        core.register_alias(modname .. ":wood","moretrees:apple_tree_planks")
+        core.register_alias(modname .. ":leaves","default:leaves")
+        core.register_alias(modname .. ":fence","default:fence_wood")
+        core.register_alias(modname .. ":fence_rail","default:fence_rail_wood")
 
-        minetest.register_alias("doors:door_" .. modname .. "_wood_a", "doors:door_wood_a")
-        minetest.register_alias("doors:door_" .. modname .. "_wood_b", "doors:door_wood_b")
-        minetest.register_alias("doors:door_" .. modname .. "_wood_c", "doors:door_wood_c")
-        minetest.register_alias("doors:door_" .. modname .. "_wood_d", "doors:door_wood_d")
+        core.register_alias("doors:door_" .. modname .. "_wood_a", "doors:door_wood_a")
+        core.register_alias("doors:door_" .. modname .. "_wood_b", "doors:door_wood_b")
+        core.register_alias("doors:door_" .. modname .. "_wood_c", "doors:door_wood_c")
+        core.register_alias("doors:door_" .. modname .. "_wood_d", "doors:door_wood_d")
 
-        minetest.register_alias(modname .. ":gate_open", "default:gate_wood_open")
-        minetest.register_alias(modname .. ":gate_closed", "default:gate_wood_closed")
+        core.register_alias(modname .. ":gate_open", "default:gate_wood_open")
+        core.register_alias(modname .. ":gate_closed", "default:gate_wood_closed")
 
-        minetest.register_alias_force("stairs:stair_" .. modname .. "_trunk", "moretrees:stair_apple_tree_planks")
-		minetest.register_alias_force("stairs:stair_outer_" .. modname .. "_trunk", "moretrees:stair_apple_tree_planks_outer")
-		minetest.register_alias_force("stairs:stair_inner_" .. modname .. "_trunk", "moretrees:stair_apple_tree_planks_inner")
-		minetest.register_alias_force("stairs:slab_"  .. modname .. "_trunk", "moretrees:slab_apple_tree_planks")
+        core.register_alias_force("stairs:stair_" .. modname .. "_trunk", "moretrees:stair_apple_tree_planks")
+		core.register_alias_force("stairs:stair_outer_" .. modname .. "_trunk", "moretrees:stair_apple_tree_planks_outer")
+		core.register_alias_force("stairs:stair_inner_" .. modname .. "_trunk", "moretrees:stair_apple_tree_planks_inner")
+		core.register_alias_force("stairs:slab_"  .. modname .. "_trunk", "moretrees:slab_apple_tree_planks")
 
         stairsplus:register_alias_all(modname, "wood", "moretrees", "apple_tree_planks")
     end
@@ -87,14 +84,14 @@ local baditemstrings = {
 for _,itemstring in pairs(baditemstrings) do
     local itemstringsplit = itemstring:split(":")
 
-    if not minetest.get_modpath(itemstringsplit[1]) then
-        minetest.register_alias(itemstring, "air")
+    if not core.get_modpath(itemstringsplit[1]) then
+        core.register_alias(itemstring, "air")
     end
 end
 
-if not minetest.get_modpath("palm") then
-    minetest.register_alias("doors:door_palm_a", "doors:door_wood_a")
-    minetest.register_alias("doors:door_palm_b", "doors:door_wood_b")
-    minetest.register_alias("doors:door_palm_c", "doors:door_wood_c")
-    minetest.register_alias("doors:door_palm_d", "doors:door_wood_d")
+if not core.get_modpath("palm") then
+    core.register_alias("doors:door_palm_a", "doors:door_wood_a")
+    core.register_alias("doors:door_palm_b", "doors:door_wood_b")
+    core.register_alias("doors:door_palm_c", "doors:door_wood_c")
+    core.register_alias("doors:door_palm_d", "doors:door_wood_d")
 end
